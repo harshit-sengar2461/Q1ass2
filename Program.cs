d58bc28da965dfebf28a06f691d1e99e517ed19f
harsh <harsh@HARSHIT-246201U>
2023-08-21 15:18:20 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Employee
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public double Salary { get; set; }
+
+   
+    public Employee(string name, int age, double salary)
+    {
+        Name = name;
+        Age = age;
+        Salary = salary;
+    }
+
+    
+    public void DisplayDetails()
+    {
+        Console.WriteLine("Employee Name: " + Name);
+        Console.WriteLine("Employee Age: " + Age);
+        Console.WriteLine("Employee Salary: $" + Salary);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Employee employee1 = new Employee("Harshit Sengar", 22, 50000000.00);
+
+        
+        Console.WriteLine("Employee Details:");
+        employee1.DisplayDetails();
+    }
+}
+
+
